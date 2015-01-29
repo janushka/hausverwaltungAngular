bec8f93b1e456e335c9ef6cab273e647dfe9e966
janushka <marc.njoku@icloud.com>
2015-01-29 17:11:57 +0100

    - added rhaboo-Library.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,17 +15,23 @@ angular.module('hausverwaltungAngularApp')
       'Karma'
     ];
 
-    $scope.rowBookingCollection = [
-      {date: '24.01.2015', remark: 'PHP Magazin 02.2015', category: 'Programming', amount: 9.80},
-      {date: '15.01.2015', remark: 'NAD CD-Player', category: 'HiFi', amount: 315},
-      {date: '13.12.2014', remark: 'Moscot Brille', category: 'Gesundheit', amount: 572},
-      {date: '16.01.2015', remark: 'Sandvquist Tasche', category: 'Kleidungen', amount: 169},
-      {date: '20.01.2015', remark: 'Brooks Laufschuhe', category: 'Kleidungen', amount: 103.95}
-    ];
+    var store = Rhaboo.persistent('BOOKINGS');
 
-   /*for (var i=0; i<$scope.rowBookingCollection.length; i++) {
-     localStorageService.set('booking' + i ,JSON.stringify($scope.rowBookingCollection[i]));
-   }*/
+    var bookingsInStore = [];
+
+    var keys = localStorageService.keys();
+    var counter = keys.length;
+
+    /*for (var i = 0; i < counter; i++) {
+      var temp = localStorageService.get(keys[i]);
+      bookingsInStore.push(localStorageService.get(keys[i]));
+    }*/
+
+    //bookingsInStore =
+
+    //$scope.rowBookingCollection = bookingsInStore || [];
+    $scope.rowBookingCollection = store.bookings || [];
 
     $scope.displayedBookingCollection = [].concat($scope.rowBookingCollection);
+
   });
--- app/scripts/controllers/newBooking.js
+++ app/scripts/controllers/newBooking.js
@@ -8,10 +8,45 @@
  * Controller of the hausverwaltungAngularApp
  */
 angular.module('hausverwaltungAngularApp')
-  .controller('NewBookingController', function ($scope) {
+  .controller('NewBookingController', function ($scope, localStorageService) {
+    var store = Rhaboo.persistent('BOOKINGS');
+    store.write('bookingCounter', store.bookingCounter ? store.bookingCounter : 0);
+    store.write('bookings', []);
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.submitBooking = function () {
+      $scope.newBooking = {
+        amount: $scope.amount,
+        date: $scope.date,
+        category: $scope.category,
+        remark: $scope.remark
+      };
+    };
+
+    $scope.$watch('newBooking', function (newValue, oldValue) {
+      //localStorageService.set('booking' + ++counter, $scope.newBooking);
+      if (newValue !== oldValue) {
+        //var tempNewBooking = $scope.newBooking;
+        // Only increment the counter if the value changed
+        //console.log('Variable newBooking has changed. It\'s new value: ' + JSON.stringify($scope.newBooking));
+        //$scope.counter = $scope.counter + 1;
+        //localStorageService.set('booking' + $scope.counter ,JSON.stringify($scope.newBooking));
+        var nextBookingNumber = store.bookingCounter + 1;
+
+        var booking = {};
+        booking.amout = $scope.newBooking.amount;
+        booking.date = $scope.newBooking.date;
+        booking.remark = $scope.newBooking.remark;
+        booking.category = $scope.newBooking.category;
+
+        var tim = "TIM";
+
+        store.bookings.write(nextBookingNumber, booking);
+      }
+    }, true);
   });
