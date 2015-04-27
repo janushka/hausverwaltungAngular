a57cc44a40cfb7638d69955cc838f3d841696229
janushka <marc.njoku@icloud.com>
2015-04-27 17:41:06 +0200

    - added angular-pouch-db module (needs to be installed) - added moment-angular module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,35 +9,31 @@
  * Main module of the application.
  */
 angular
-  .module('hausverwaltungAngularApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'smart-table',
-    'LocalStorageModule',
-    'pouchdb'
-  ])
-  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
-    localStorageServiceProvider.setPrefix('ls');
-  }])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainController'
-      })
-      .when('/new_booking', {
-        templateUrl: 'views/newBooking.html',
-        controller: 'NewBookingController'
-      })
-      .when('/about', {
-        templateUrl: 'views/newCategory.html',
-        controller: 'NewCategoryController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('hausverwaltungAngularApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'smart-table',
+        'pouchdb'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainController'
+            })
+            .when('/new_booking', {
+                templateUrl: 'views/newBooking.html',
+                controller: 'NewBookingController'
+            })
+            .when('/new_category', {
+                templateUrl: 'views/new_category.html',
+                controller: 'NewCategoryCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,25 +8,27 @@
  * Controller of the hausverwaltungAngularApp
  */
 angular.module('hausverwaltungAngularApp')
-  .controller('MainController', function ($scope, localStorageService) {
+    .controller('MainController', function ($scope, dbService) {
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+        dbService.initDb(false);
 
-    $scope.rowBookingCollection = [
-      {date: '24.01.2015', remark: 'PHP Magazin 02.2015', category: 'Programming', amount: 9.80},
-      {date: '15.01.2015', remark: 'NAD CD-Player', category: 'HiFi', amount: 315},
-      {date: '13.12.2014', remark: 'Moscot Brille', category: 'Gesundheit', amount: 572},
-      {date: '16.01.2015', remark: 'Sandvquist Tasche', category: 'Kleidungen', amount: 169},
-      {date: '20.01.2015', remark: 'Brooks Laufschuhe', category: 'Kleidungen', amount: 103.95}
-    ];
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
 
-    /*for (var i=0; i<$scope.rowBookingCollection.length; i++) {
-     localStorageService.set('booking' + i ,JSON.stringify($scope.rowBookingCollection[i]));
-     }*/
+        $scope.rowBookingCollection = [
+            {date: '24.01.2015', remark: 'PHP Magazin 02.2015', category: 'Programming', amount: 9.80},
+            {date: '15.01.2015', remark: 'NAD CD-Player', category: 'HiFi', amount: 315},
+            {date: '13.12.2014', remark: 'Moscot Brille', category: 'Gesundheit', amount: 572},
+            {date: '16.01.2015', remark: 'Sandvquist Tasche', category: 'Kleidungen', amount: 169},
+            {date: '20.01.2015', remark: 'Brooks Laufschuhe', category: 'Kleidungen', amount: 103.95}
+        ];
 
-    $scope.displayedBookingCollection = [].concat($scope.rowBookingCollection);
-  });
+        /*for (var i=0; i<$scope.rowBookingCollection.length; i++) {
+         localStorageService.set('booking' + i ,JSON.stringify($scope.rowBookingCollection[i]));
+         }*/
+
+        $scope.displayedBookingCollection = [].concat($scope.rowBookingCollection);
+    });
